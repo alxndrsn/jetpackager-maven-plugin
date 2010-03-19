b0f8f32a043d84c145944c299b2875f87873d67f
Alex Anderson <alex@frontlinesms.com>
2010-03-19 12:38:11 +0000

    Extended the mojo to call pack and compile.

--- src/main/java/net/frontlinesms/build/jet/maven/JetBuildMojo.java
+++ src/main/java/net/frontlinesms/build/jet/maven/JetBuildMojo.java
@@ -1,5 +1,13 @@
 package net.frontlinesms.build.jet.maven;
 
+import java.io.File;
+import java.io.IOException;
+
+import net.frontlinesms.build.jet.compile.JetCompileProfile;
+import net.frontlinesms.build.jet.compile.JetCompiler;
+import net.frontlinesms.build.jet.pack.JetPackProfile;
+import net.frontlinesms.build.jet.pack.JetPacker;
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -26,13 +34,49 @@ public class JetBuildMojo extends AbstractMojo {
 		getLog().info("...JetBuild complete.");
 	}
 
-	private void doPack() {
-		// TODO Auto-generated method stub
-		
+//> COMPILE METHODS
+	private void doCompile() throws JetCompileException {
+		JetCompiler compiler = new JetCompiler(new File("target/jet-packager/compile"));
+		compiler.configureDefaults();
+		try {
+			compiler.doCompile(getCompileProfile());
+		} catch (IOException ex) { throw new JetCompileException("Exception doing jet compile.", ex); }
 	}
 
-	private void doCompile() {
+	private JetCompileProfile getCompileProfile() {
 		// TODO Auto-generated method stub
-		
+		return null;
+	}
+
+//> PACKING METHODS
+	private void doPack() throws JetPackException {
+		JetPacker packer = new JetPacker(new File("target/jet-packager/compile"));
+		packer.configureDefaults();
+		try {
+			packer.doPack(getPackProfile());
+		} catch (IOException ex) { throw new JetPackException("Exception doing jet pack.", ex); }
+	}
+
+	private JetPackProfile getPackProfile() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
+
+abstract class JetBuildException extends MojoExecutionException {
+	public JetBuildException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
+
+class JetCompileException extends JetBuildException {
+	public JetCompileException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
+
+class JetPackException extends JetBuildException {
+	public JetPackException(String message, Throwable cause) {
+		super(message, cause);
 	}
 }
